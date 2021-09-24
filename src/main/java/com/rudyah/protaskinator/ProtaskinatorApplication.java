31500ce05a22d3ac72bc66e0eac7915ee3e8e617
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-09-23 23:05:12 -0700

    Initial commit

---
+++ src/main/java/com/rudyah/protaskinator/ProtaskinatorApplication.java
@@ -0,0 +1,13 @@
+package com.rudyah.protaskinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtaskinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtaskinatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rudyah/protaskinator/ProtaskinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudyah.protaskinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtaskinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
