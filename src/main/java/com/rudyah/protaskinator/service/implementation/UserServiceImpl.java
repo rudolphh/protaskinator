aa7641fa82a4c425f4a597df6dd190ce5cae5d15
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-09-24 00:57:30 -0700

    update UserServiceImpl

--- src/main/java/com/rudyah/protaskinator/service/implementation/UserServiceImpl.java
+++ src/main/java/com/rudyah/protaskinator/service/implementation/UserServiceImpl.java
@@ -20,16 +20,19 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public AppUser saveUser(AppUser user) {
+        log.info("Saving new user {} to the database", user.getName());
         return userRepo.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
+        log.info("Saving new role {} to the database", role.getName());
         return roleRepo.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
+        log.info("Adding role {} to user {}", roleName, username);
         AppUser user = userRepo.findByUsername(username);
         Role role = roleRepo.findByName(roleName);
         user.getRoles().add(role);
@@ -37,11 +40,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public AppUser getUser(String username) {
+        log.info("Fetching user {}", username);
         return userRepo.findByUsername(username);
     }
 
     @Override
     public Page<AppUser> getUsers(int pageNumber, int pageSize) {
+        log.info("Fetching all users");
         Pageable pageable = PageRequest.of(pageNumber, pageSize);
         return userRepo.findAll(pageable);
     }
