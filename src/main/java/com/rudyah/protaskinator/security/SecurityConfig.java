a8149b97b6f343ce43c5ca024dc183c4085cc0ee
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-09-24 20:56:29 -0700

    implement authorization

--- src/main/java/com/rudyah/protaskinator/security/SecurityConfig.java
+++ src/main/java/com/rudyah/protaskinator/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.rudyah.protaskinator.security;
 
 import com.rudyah.protaskinator.filter.CustomAuthenticationFilter;
+import com.rudyah.protaskinator.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
@@ -41,6 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().anyRequest().authenticated();
 
         http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
